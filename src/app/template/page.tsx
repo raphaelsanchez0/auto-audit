8db868fbb75d76672564d57beb8e3860768107ba
raphaelsanchez0 <sanchezraphael0@gmail.com>
2025-09-27 15:32:37 -0400

    add prisma orm and ui elements

---
+++ src/app/template/page.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React from "react";
+
+export default function TemplatePage() {
+  return (
+    <div className="w-8/12 mx-auto my-10">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-4xl font-bold tracking-tight text-center">
+            Templates
+          </CardTitle>
+          <CardDescription className="text-center mt-2">
+            Templates are an reuseable way to handle your government specs. Get
+            started by making one below
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-center">
+          <Button className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Create Template
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
