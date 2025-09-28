5f0ae9933a6a2d9a7a13f8d677ce9cd55e2991aa
raphaelsanchez0 <sanchezraphael0@gmail.com>
2025-09-27 21:49:49 -0400

    feat: audit page

--- src/app/audit/[templateId]/page.tsx
+++ src/app/audit/[templateId]/page.tsx
@@ -6,9 +6,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
 
 import React, { use } from "react";
 
@@ -31,21 +33,45 @@ export default function AuditPage({
             <Separator />
           </div>
           <CardContent className="space-y-2">
-            <CardTitle className="text-gray-600">Proof of Compliance</CardTitle>
+            <CardTitle className="">Proof of Compliance</CardTitle>
             <CardDescription>
               Evidence that the requirement has been met. You can submit text, a
               screenshot, or a PDF document.
             </CardDescription>
-            <div className="flex w-full max-w-sm flex-col gap-6">
+            <div className="flex w-full flex-col gap-6">
               <Tabs defaultValue="text" className="w-full">
                 <TabsList>
                   <TabsTrigger value="text">Text</TabsTrigger>
                   <TabsTrigger value="screenshot">Screenshot</TabsTrigger>
                   <TabsTrigger value="document">Document</TabsTrigger>
                 </TabsList>
+                <TabsContent value="text" className="w-full">
+                  <Textarea className="w-full" />
+                </TabsContent>
+                <TabsContent value="screenshot" className="w-full">
+                  <div>
+                    <Input type="file" accept="image/*" />
+                  </div>
+                </TabsContent>
+                <TabsContent value="document" className="w-full">
+                  <div>
+                    <Input type="file" accept="application/pdf" />
+                  </div>
+                </TabsContent>
               </Tabs>
             </div>
+            <CardTitle className="mt-6">Additional Context</CardTitle>
+            <CardDescription>
+              Any additional information that may be relevant proof for the
+              audit.
+            </CardDescription>
+            <Textarea className="w-full" />
           </CardContent>
+        </Card>
+        <Card className="m-4">
+          <CardHeader>
+            <CardTitle className="text-xl">Feedback</CardTitle>
+          </CardHeader>
         </Card>
       </div>
     </div>
